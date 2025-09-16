2eea985ef3cb8ece4be4e2506ae5400e5d08e588
Finney Student <student@finney.com>
2025-09-16 19:52:50 -0400

    Started adding cave class.

---
+++ src/Cave.java
@@ -0,0 +1,7 @@
+/**
+ * Each room inthe cave has a unique ID (1-28) and is arranged 
+ * in the shape of a dodecahedron.
+ */
+public class Cave {
+private Map<Integer, int[]
+}
--- src/HuntTheWumpus.java
+++ src/HuntTheWumpus.java
@@ -1,5 +1,5 @@
 public class HuntTheWumpus {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+System.out.println(x:"Hello World!");
     }
 }

8fcf8b4a860394b579a9bececdbd34f828368f9b
Chris McDonald <McDonaldChristopherC@gmail.com>
2025-09-15 20:24:11 -0400

    add the Cave add Cave test code

--- src/HuntTheWumpus.java
+++ src/HuntTheWumpus.java
@@ -1,5 +1,15 @@
 public class HuntTheWumpus {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create the cave
+        Cave cave = new Cave();
+
+        // print out each room and it's connections
+        for (int roomID : cave.getAllRooms()) {
+            System.out.print("Room\t" + roomID);
+            for (int path : cave.getPaths(roomID)) {
+                System.out.print("\t" + path);
+            }
+            System.out.println();
+        }
     }
 }

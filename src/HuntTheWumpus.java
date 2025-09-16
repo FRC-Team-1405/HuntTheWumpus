ef51fe63230bc69c1c5bdd5a5278b55b52f592c8
Finney Student <Finney Student>
2025-09-16 19:51:32 -0400

    Started Cave Class

--- src/HuntTheWumpus.java
+++ src/HuntTheWumpus.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class HuntTheWumpus {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] myArray = new int[] { 18, 99, 34 }
+        List<Integer> lst; = new Has
+        List<Boolean> lst2;
+     map<String, Integer> map = HashMap<String, Integer>   
     }
 }

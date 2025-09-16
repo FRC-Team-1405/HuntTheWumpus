a96bcf43bc07d274446a49456e1255da5bdbc79c
Finney Student <student@finney.com>
2025-09-16 19:51:41 -0400

    Started adding Cave class

---
+++ src/Cave.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+     * Each room in the cave has a unique ID (1-20) and is arranged
+     * in the shape of a dodecahedron.
+     */
+    public class Cave {
+        private Map<Integer, int[]> rooms = new HashMap<Integer, int[]>();
+
+        public Cave() {
+            
+        }
+}

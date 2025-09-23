d00d6bb39e18b54621ab6a7b33959af8bb882c24
Finney Student <Finney Student>
2025-09-23 19:45:44 -0400

    worked more on rooms

--- src/Cave.java
+++ src/Cave.java
@@ -9,5 +9,27 @@ public class Cave {
     private Map<Integer, int[]> rooms = new HashMap<Integer, int[]>();
 
     public Cave() {
+        rooms.put(Integer.valueOf(1), new int[] { 5, 8, 2 });
+        rooms.put(Integer.valueOf(2), new int[] { 1, 10, 3 });
+        rooms.put(Integer.valueOf(3), new int[] { 2, 12, 4 });
+    }
+
+    public int[] getpaths(int roomID) {
+        return rooms.get(Integer.valueOf(roomID));
+    }
+
+    public boolean isPathValid(int fromID, int toID) {
+        int[] paths = rooms.get(fromID);
+
+        if (paths == null) {
+            return false;
+        }
+
+        for (int destination : paths) {
+            if (destination == toID) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/HuntTheWumpus.java
+++ src/HuntTheWumpus.java
@@ -1,5 +1,15 @@
 public class HuntTheWumpus {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cave cave = new Cave();
+        int[] rooms = cave.getpaths(1);
+
+        boolean pathExists = cave.isPathValid(1, 5);
+
+        System.out.println(rooms[0]);
+        System.out.println(rooms[1]);
+        System.out.println(rooms[2]);
+
+        System.out.println("Hello");
+
     }
 }

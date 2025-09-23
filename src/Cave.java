5993ba145de619caa5748cd48050976fb4868ca4
Finney Student <student@finney.com>
2025-09-23 19:40:17 -0400

    added a function

--- src/Cave.java
+++ src/Cave.java
@@ -2,13 +2,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
-     * Each room in the cave has a unique ID (1-20) and is arranged
-     * in the shape of a dodecahedron.
-     */
-    public class Cave {
-        private Map<Integer, int[]> rooms = new HashMap<Integer, int[]>();
+ * Each room in the cave has a unique ID (1-20) and is arranged
+ * in the shape of a dodecahedron.
+ */
+public class Cave {
+    private Map<Integer, int[]> rooms = new HashMap<Integer, int[]>();
 
-        public Cave() {
-            
+    public Cave() {
+        rooms.put(Integer.valueOf(1), new int[] { 5, 8, 2 });
+        rooms.put(Integer.valueOf(2), new int[] { 1, 10, 3 });
+        rooms.put(Integer.valueOf(3), new int[] { 2, 12, 4 });
+    }
+
+    public int[] getPaths(int roomID) {
+        return rooms.get(Integer.valueOf(roomID));
+
+    }
+
+    public boolean isPathValid(int fromID, int toID) {
+        int[] paths = rooms.get(fromID);
+
+        if (paths == null)
+            return false;
+
+        for (int destination : paths) {
+            if (destination == toID)
+                return true;
         }
+        return false;
+    }
 }
--- src/HuntTheWumpus.java
+++ src/HuntTheWumpus.java
@@ -1,6 +1,19 @@
 public class HuntTheWumpus {
-    public static void main(String[] args) {
 
-        
+    public static void main(String[] args) {
+        Cave cave = new Cave();
+        int[] rooms = cave.getPaths(1);
+
+        boolean pathExists = cave.isPathValid(1, 5);
+        System.out.println(rooms[0]);
+        System.out.println(rooms[1]);
+        System.out.println(rooms[2]);
+
+        System.out.println(cave.getPaths(0));
+        System.out.println(cave.getPaths(1));
+        System.out.println(cave.getPaths(2));
+
+        System.out.println("Hello World");
+
     }
 }
